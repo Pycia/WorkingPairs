d015e4576f0f58c525fbd954d15f37762f0ba501
Natalia Stawowy <nataliastawowy@gmail.com>
2014-01-25 12:16:54 +0100

    Scalenie plików .h fo utils.h

--- utils.h
+++ utils.h
@@ -1,4 +1,38 @@
 #include <iostream>
+#include <stdlib.h> 
+#include <time.h>
+int* generator()
+{
+  int tab[10];
+  for(int i=0; i<10; i++)
+  {
+	  int j; 
+	  srand(time(0)); 
+	  tab[i] = rand()%101; 
+  }
+  return tab;
+}
+int max(int * tab)
+{
+	int max = tab[0];
+	for(int i=0; i<10; i++)
+	{
+		if(tab[i]>max)
+			max=tab[i];
+	}
+	return max;
+}
+
+int min(int * tab)
+{
+	int min = tab[0];
+	for(int i=0; i<10; i++)
+	{
+		if(tab[i]<min)
+			min=tab[i];
+	}
+	return min;
+}
 
 int* printTable(int *tab){
 
